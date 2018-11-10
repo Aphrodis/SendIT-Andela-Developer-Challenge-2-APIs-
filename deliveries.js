b0e5c24c25fde2fc15f6f2c43635ab4f3a79faae
Aphrodice Imanizabayo <34691089+Aphrodis@users.noreply.github.com>
2018-11-10 10:14:15 +0200

    Update deliveries.js

--- deliveries.js
+++ deliveries.js
@@ -9,10 +9,10 @@ app.get('/',(req,res)=>{
 });
 const parcels = [
 	{id: 1, owner: 'Aphrodice', pickup:'Rwanda', destination: 'Ghana', weight: 12, price: 14400},
-	{id: 2, owner: 'Ariel', pickup:'Nigeria', destination: 'DRC', weight: 7, price: 12800},
-	{id: 3, owner: 'Emile', pickup:'Tanzania', destination: 'Eritrea', weight: 23, price: 19700},
-	{id: 4, owner: 'Honorine', pickup:'South Africa', destination: 'Zambia', weight: 18, price: 16200},
-	{id: 5, owner: 'Jesca', pickup:'Egypt', destination: 'Rwanda', weight: 32, price: 21000}
+	{id: 2, owner: 'Ariel', pickup:'Nigeria', destination: 'DRC', weight: 7, price: 8400},
+	{id: 3, owner: 'Emile', pickup:'Tanzania', destination: 'Eritrea', weight: 23, price: 27600},
+	{id: 4, owner: 'Honorine', pickup:'South Africa', destination: 'Zambia', weight: 18, price: 21600},
+	{id: 5, owner: 'Jesca', pickup:'Egypt', destination: 'Rwanda', weight: 32, price: 38400}
 ];
 
 
